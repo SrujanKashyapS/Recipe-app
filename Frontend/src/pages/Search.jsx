33d6b55650dc256cbbec2126255260c4f702ac13
Suhas-Tatte <suhastatte17@gmail.com>
2024-11-18 18:37:43 +0530

    changes in search page

--- Frontend/src/pages/Search.jsx
+++ Frontend/src/pages/Search.jsx
@@ -13,7 +13,7 @@ const Search = () => {
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
-    const queryParam =   params.get('query');
+    const queryParam = params.get('query');
     if(queryParam){
       setQuery(queryParam);
     }
@@ -22,7 +22,7 @@ const Search = () => {
     const fetchItems = async () => {
       setLoading(true);
       try {
-        const response = await axios.get(`http://localhost:5000/api/items/`,{
+        const response = await axios.get(`http://localhost:5000/api/items`,{
           params: {q: query}
         });
         setResults(response.data);
@@ -34,18 +34,19 @@ const Search = () => {
     }
     fetchItems();
   },[query])
-  console.log(results) //Testing
+  
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+  }
   
   return (
     <div className='px-6 lg:px-12 py-20'>
       <h1 className='text-center text-3xl md:max-w-3xl mx-auto py-10 font-semibold text-secondary sm:text-6xl sm:leading-relaxed'>Search</h1>
       <div className='bg-white p-4 rounded relative flex items-center'>
-          <form action='/search' className='bg-white p-4 rounded relative flex items-center'>
             <IoSearchSharp className='w-5 h-5 mr-2 text-neutral-300'/>
-            <input name='query' type='search' placeholder='Search for a Recipe' className='outline-none w-full placeholder:text-[#1b2629]' 
+            <input value={query} onChange={handleSearch} type='search' placeholder='Search for a Recipe' className='outline-none w-full placeholder:text-[#1b2629]' 
             id='search' required=""/>
-          </form>
-        </div>
+      </div>
         {loading && <div>Loading...</div>}
         {error && <div>Unknown error happens...{error}</div>}
         <ul className='mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
